4b174cba70b245080d6e42ac3da2089e18b621a1
SaidganiUsm <saidgani110@gmail.com>
2023-08-11 16:02:27 +0500

    .

---
+++ Notes.Application/Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Commands.DeleteCommand
+{
+    public class DeleteNoteCommandHandler
+        : IRequestHandler<DeleteNoteCommand, Unit>
+    {
+        private readonly INotesDbContext _context;
+
+        public DeleteNoteCommandHandler(INotesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteNoteCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Notes.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundExcemption(nameof(Note), request.Id);
+            }
+
+            _context.Notes.Remove(entity);
+            _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
