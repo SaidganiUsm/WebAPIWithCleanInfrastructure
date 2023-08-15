3239846b5bde8ce911541a2f5ee27beda142e436
SaidganiUsm <saidgani110@gmail.com>
2023-08-15 15:06:59 +0500

    Added services for Depedency Injection

--- Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
+++ Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Domain;
 
 namespace Notes.Application.Notes.Queries.GetNoteDetails
 {
-    internal class NoteDetailsVm
-    {
+    public class NoteDetailsVm : IMapWith<Note>
+    { 
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+
+        public void Mapping(Profile profile) 
+        {
+             profile.CreateMap<Note, NoteDetailsVm>()
+                .ForMember(n => n.Title, o => o.MapFrom(note => note.Title))
+                .ForMember(n => n.Details, o => o.MapFrom(note => note.Details))
+                .ForMember(n => n.Id, o => o.MapFrom(note => note.Id))
+                .ForMember(n => n.CreationDate, o => o.MapFrom(note => note.CreationDate))
+                .ForMember(n => n.EditDate, o => o.MapFrom(note => note.EditDate));
+        }
     }
 }
--- Notes.Application/Notes/Queries/GetNoteList/NoteLookupDto.cs
+++ Notes.Application/Notes/Queries/GetNoteList/NoteLookupDto.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Domain;
 
 namespace Notes.Application.Notes.Queries.GetNoteList
 {
-    internal class NoteLookupDto
+    public class NoteLookupDto : IMapWith<Note>
     {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteDetailsVm>()
+                .ForMember(n => n.Id, o => o.MapFrom(note => note.Id))
+                .ForMember(n => n.Title, o => o.MapFrom(note => note.Title));
+        }
     }
 }
